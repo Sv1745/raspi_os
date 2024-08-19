505364ba6ddc303708c2254230c41b0ce83b4694
Srivathsa <66905185+Sv1745@users.noreply.github.com>
2024-08-19 17:33:15 +0530

    Add files via upload

---
+++ dep1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update and Upgrade the system
+sudo apt update
+sudo apt-get upgrade -y
+
+# Install Python packages
+sudo pip3 install flask --break-system-packages
+sudo pip3 install sqlalchemy==1.3.23 --break-system-packages
+sudo pip install flask_sqlalchemy==2.5.1 --break-system-packages
+sudo pip3 install flask-cors --break-system-packages
+sudo pip3 install -U pymodbus==2.5.3 --break-system-packages
+sudo pip3 install schedule --break-system-packages
+sudo pip3 install aiohttp --break-system-packages
+
+# Install Node.js and global npm packages
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs 
+sudo npm install -g serve
+
+# Check versions of npm and node
+npm -v
+node -v
+
+# Install SQLite and Apache2
+sudo apt-get install sqlite3 -y
+sudo apt-get install sqlitebrowser -y
+sudo apt install apache2 -y
+
+# Screen resolution settings dependencies
+sudo apt-get install xserver-xorg-input-libinput -y
+sudo mkdir /etc/X11/xorg.conf.d 
+sudo cp /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/
+sudo nano /etc/X11/xorg.conf.d/40-libinput.conf
